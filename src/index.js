9a646ea0c0f36bc98dbb0da69eedda21af1c37b0
Ian Mackenzie <ian@mackenzie.work>
2025-10-14 15:25:00 -0700

    switch to webgl

--- src/index.js
+++ src/index.js
@@ -1,31 +1,12 @@
+main()
 
-const width = window.innerWidth, height = window.innerHeight;
 
-// init
+function main() {
+  if (gl === null) {
+    alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+    return;
+  }
 
-const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
-camera.position.z = 1;
-
-const scene = new THREE.Scene();
-
-const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-const material = new THREE.MeshNormalMaterial();
-
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
-
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(width, height);
-renderer.setAnimationLoop(animate);
-document.body.appendChild(renderer.domElement);
-
-// animation
-
-function animate(time) {
-
-  mesh.rotation.x = time / 2000;
-  mesh.rotation.y = time / 1000;
-
-  renderer.render(scene, camera);
-
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
+  gl.clear(gl.COLOR_BUFFER_BIT);
 }
