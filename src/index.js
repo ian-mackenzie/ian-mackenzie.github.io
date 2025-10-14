c2107de24a204c1cd557ce945e3e7d68d8aceaa2
Ian Mackenzie <ian@mackenzie.work>
2025-10-14 15:19:22 -0700

    move import to cdn

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@ const width = window.innerWidth, height = window.innerHeight;
 
 // init
 
-const camera = new three.PerspectiveCamera(70, width / height, 0.01, 10);
+const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
 camera.position.z = 1;
 
-const scene = new three.Scene();
+const scene = new THREE.Scene();
 
-const geometry = new three.BoxGeometry(0.2, 0.2, 0.2);
-const material = new three.MeshNormalMaterial();
+const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+const material = new THREE.MeshNormalMaterial();
 
-const mesh = new three.Mesh(geometry, material);
+const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
-const renderer = new three.WebGLRenderer({ antialias: true });
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(width, height);
 renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
